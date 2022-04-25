a33e3fe6f4f130e5ea348c64c196b632e021926d
Max Fang <protonmail.com@maxfa.ng>
2022-04-25 14:53:49 -0700

    SyncSocketDescriptor skeleton implementation

--- lightning-net/src/lib.rs
+++ lightning-net/src/lib.rs
@@ -7,6 +7,48 @@
 // You may not use this file except in accordance with one or both of these
 // licenses.
 
-#![deny(broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
 
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+#![allow(dead_code)] // TODO: Remove when complete
+
+use core::hash;
+use std::sync::{Arc, Mutex};
+
+use lightning::ln::peer_handler::SocketDescriptor;
+
+/// An synchronous SocketDescriptor (i.e. it doesn't rely on Tokio)
+#[derive(Clone)]
+struct SyncSocketDescriptor {
+    id: u16,
+    outbound_data: Arc<Mutex<Vec<u8>>>,
+}
+impl PartialEq for SyncSocketDescriptor {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+impl Eq for SyncSocketDescriptor { }
+impl hash::Hash for SyncSocketDescriptor {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state)
+    }
+}
+
+impl SocketDescriptor for SyncSocketDescriptor {
+    fn send_data(&mut self, _data: &[u8], _resume_read: bool) -> usize {
+        unimplemented!();
+    }
+
+    fn disconnect_socket(&mut self) {
+        unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn hello_test() {
+        assert!(true);
+    }
+}
